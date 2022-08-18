cee1fb9b1c1461792a3c7fd1c1fc1b03c158a20e
Mikhail Toropchinov <46166975+ruhose73@users.noreply.github.com>
2022-08-18 03:56:39 +0300

    Init Swagger config

--- AuthService/app.js
+++ AuthService/app.js
@@ -5,15 +5,42 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 5000;
 const router = require("./routes/routes");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const errorMiddleware = require("./middlewares/errorMiddleware");
+
+const swaggerConfig = {
+
+    definition: {
+        openapi: "3.0.0",
+        info: {
+        title: "Notes",
+        version: "1.0.0",
+        description: "Сервис заметок",
+        },
+        servers: [
+        {
+            url: `${process.env.API_FULL_URL}`
+        },
+        ],
+    },
+
+    apis: ["./docs/*/*.js", "./docs/*.js"],
+};
+
+const swaggerSpecs = swaggerJsDoc(swaggerConfig);
 
 const app = express();
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
+//  http://localhost:5000/docs
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
+
 //  http://localhost:5000/notes
 app.use("/notes", router);
+
 app.use(errorMiddleware);
 
 const start = async () => {
