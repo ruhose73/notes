8e8c56307a0abde4bbf3bb857a47d445a7e77f78
Mikhail Toropchinov <46166975+ruhose73@users.noreply.github.com>
2022-08-18 03:37:15 +0300

    AuthMiddleware and some fixes

--- AuthService/middlewares/authMiddleware.js
+++ AuthService/middlewares/authMiddleware.js
@@ -4,26 +4,22 @@ const TokenService = require("../services/tokenService");
 module.exports = function (req, res, next) {
 
     try {
-        
-        //Если токен есть в куках
-        if (req.cookies !== null) {
-            const accessToken = req.cookies;
+        if (req.cookies.accessToken != null) {
+            const accessToken = req.cookies.accessToken;
             const userData = TokenService.validateAcessToken(accessToken)
             if (!userData) {
                 return next(ApiError.UnauthorizedError());
             }
             req.user = userData
             next()
-
-        //если токена нет в куках
         } else {
             const accessToken = req.headers.authorization != null ? req.headers.authorization.split(' ')[1] : null
             const userData = TokenService.validateAcessToken(accessToken)
             if (!userData) {
                 return next(ApiError.UnauthorizedError());
             }
-            req.user = userData
-            next()
+            req.user = userData;
+            next();
         }
 
     } catch (e) {
